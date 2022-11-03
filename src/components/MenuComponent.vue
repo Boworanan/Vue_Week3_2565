8562f22654cbc339b5621fb236af97d4fc1ad563
Boworanan <bowonranan.30@gmail.com>
2022-11-03 15:41:13 +0700

    first commit

---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>MENU</h1>
+    </div>
+</template>
+
+<script>
+    export default{}
+</script>
+
+<style lang="en">
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
+  <div >
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MenuComponent/>
+    <FooterComponent/>
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import MenuComponent from '@/components/MenuComponent.vue'
+import FooterComponent from '@/components/FooterComponent.vue';
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    MenuComponent,
+    FooterComponent,
   }
 }
 </script>
